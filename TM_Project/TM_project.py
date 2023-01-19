cf1ad5f0a6d723a836899c56ed019a4169a41025
LerBulka <119971002+LerBulka@users.noreply.github.com>
2023-01-19 10:40:46 +0200

    Add files via upload

--- TM_Project/TM_project.py
+++ TM_Project/TM_project.py
@@ -1,22 +1,103 @@
 from tkinter import *
 from tkinter import ttk
+window_size = "600x700"
+
 
 Window = Tk()
+
 Window.title("Task Manager Project")
-Window.geometry("500x665")
+
+Window.geometry(window_size)
+Window.minsize(600,700)
+Window.maxsize(600,700)
 
 tasks = []
 
 header_label = Label(
-    background = "gray", 
-    text = "To do list",
+    font = ("Consolas", "20"), 
+    text = "To-do list",
     borderwidth = 4,
+    relief = SOLID
 )
 
 task_label = ttk.Label(
-    background = "grey"
+    background = "grey",
 )
 
-header_label.place(x = 0, y = 0, width = 500, height = 100)
-task_label.place(x = 0, y = 0, width = 100, height = 600)
+addtask_button = Button(
+    background = "green",
+    foreground = "white",
+    font = ("Consolas", "15"), 
+    text = "Add Task:",
+)
+
+main_label = Label(
+    background = "lightgrey"
+)
+
+yourtasks_text_label = Label(
+    foreground = "green",
+    font = ("Consolas", "15"), 
+    text = "Your Tasks:",
+)
+
+trash_button = Button(
+    font = ("Consolas", "15"),
+    foreground = "red",
+    text = "Delete Task",
+)
+
+deleteall_button = Button(
+    font = ("Consolas", "15"), 
+    background = "red",
+    foreground = "white",
+    text = "Delete All",
+)
+
+def draw():
+
+    header_label.place(
+        x = 0, y = 0,
+        width = 600, 
+        height = 100
+    )
+
+    task_label.place(
+        x = 0, y = 100,
+        width = 200, 
+        height = 600
+    )
+
+    addtask_button.place(
+        x = 0, y = 100, 
+        width = 200, 
+        height = 50
+    )
+
+    yourtasks_text_label.place(
+        x = 200, y = 100, 
+        width = 400,
+        height = 50
+    )
+
+    main_label.place(
+        x = 200, y =100, 
+        width = 400, 
+        height = 600
+    )
+
+    trash_button.place(
+        x = 0, y =600, 
+        width = 200, 
+        height = 50
+    )
+
+    deleteall_button.place(
+        x = 0, y =650, 
+        width = 200, 
+        height = 50
+    )
+
+draw()
+
 Window.mainloop()
