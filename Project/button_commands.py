0ce571723b4b64106c9a79988f535576b950e2f5
Lexakez <lexakez@gmail.com>
2023-01-21 21:34:46 +0200

    .

--- Project/button_commands.py
+++ Project/button_commands.py
@@ -97,7 +97,7 @@ def click_task_button(window, task_button, reminder_button):
         relief = SOLID
     )
     task_name_label.place(
-        x = 200, y = 150,
+        x = 200, y = 155,
         width = 150,
         height = 30
     )
@@ -107,32 +107,71 @@ def click_task_button(window, task_button, reminder_button):
         textvariable = task_name,
     )
     entry_task_name.place(
-        x = 350, y = 150,
+        x = 350, y = 155,
         width = 250,
         height = 30
     )
     task_description_label = Label(
         text = "Description",
-        font = ("Consolas", "14")
+        font = ("Consolas", "14"),
+        relief = SOLID
     )
     task_description_label.place(
-        x = 320, y = 180,
+        x = 200, y = 190,
         width = 150,
         height = 30
     )
     task_description = StringVar()
     entry_task_description = Entry(
-        font = "14",
+        font = ("Consolas", "14"),
         textvariable = task_description
     )
     entry_task_description.place(
-        x = 200, y = 210,
-        width = 400,
+        x = 350, y = 190,
+        width = 250,
         height = 30
+    )
+    goal_label = Label(
+        text = "Goals:",
+        font = ("Consolas", "14"),
+        relief = SOLID
+    )
+    goal_label.place(
+        x = 200, y = 225,
+        width = 150,
+        height = 30
+    )
+    entry_goal = Entry(
+        font = "14",
+    )
+    entry_goal.place(
+        x = 350, y = 225,
+        width = 200,
+        height = 30
+    )
+    def add():
+        new_goal = entry_goal.get()
+        goal_list_box.insert(-1, new_goal)
+        entry_goal.delete(0, END)
+    add_button = Button(
+        text = "add",
+        command = add,
+        font = ("Consolas", "14")
+    )
+    add_button.place(
+        x = 550, y = 225,
+        width = 50,
+        height = 30
+    )
+    goal_list_box = Listbox()
+    goal_list_box.place(
+        x = 350, y = 260,
+        width = 200,
+        height = 200,
     )
     save_button = Button(
         text = "Save",
-        command = lambda: click_save_task_button(task_name, task_name_label, entry_task_name, task_header_label, save_button)
+        command = lambda: click_save_task_button(task_name, task_name_label, entry_task_name, task_header_label, save_button, add_button, task_description_label, entry_task_description, goal_label, entry_goal, goal_list_box)
     )
     save_button.place(
         x = 550, y = 660,
@@ -142,7 +181,7 @@ def click_task_button(window, task_button, reminder_button):
 
 task_list = []
 
-def click_save_task_button(task_name, task_name_label, entry_task_name, task_header_label, save_button):
+def click_save_task_button(task_name, task_name_label, entry_task_name, task_header_label, save_button, add_button, task_description_label, entry_task_description, goal_label, entry_goal, goal_list_box):
     '''Кнопка сохранить задачу'''
     task = (New_task(task_name.get()))
     task_list.append(task)
@@ -150,4 +189,10 @@ def click_save_task_button(task_name, task_name_label, entry_task_name, task_hea
     task_name_label.destroy()
     entry_task_name.destroy()
     task_header_label.destroy()
+    add_button.destroy()
+    task_description_label.destroy()
+    entry_task_description.destroy()
+    goal_label.destroy()
+    entry_goal.destroy()
+    goal_list_box.destroy()
     save_button.destroy()
